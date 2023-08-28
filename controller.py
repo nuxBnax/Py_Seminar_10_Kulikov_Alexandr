a8b4e9f38fc0ded28f4cc4eeb5fceefbed4fba46
Kulikov Alexander <shouter84@yandex.ru>
2023-08-28 22:35:09 +0300

    Added new files, fuctions and models

---
+++ controller.py
@@ -0,0 +1,51 @@
+import model
+import text
+import view
+
+
+def search_contact():
+    word = view.input_request(text.input_search_word)
+    result = model.find_contact(word)
+    view.show_book(result, text.not_find(word))
+    if result:
+        return True
+    
+
+def start():
+    while True:
+        choice = view.main_menu()
+        match choice:
+            case 1:
+                model.open_file()
+                view.print_message(text.load_successful)
+            case 2:
+                model.save_file()
+                view.print_message(text.save_successful)
+            case 3:
+                view.show_book(model.phone_book, text.empty_book_error)
+            case 4:
+                new_contact = view.input_contact(text.input_contact)
+                model.add_contact(new_contact)
+                view.print_message(text.contact_action(new_contact[0], text.operation[0]))
+            case 5:
+                search_contact()
+            case 6:
+                if search_contact():
+                    c_id = int(view.input_request(text.input_edit_contact_id))
+                    new_contact = view.input_contact(text.input_edit_contact)
+                    name = model.edit_contact(c_id, new_contact)
+                    view.print_message(text.contact_action(name, text.operation[1]))
+            case 7:
+                if search_contact():
+                    c_id = int(view.input_request(text.input_del_contact_id))
+                    name = model.delete_contact(c_id)
+                    view.print_message(text.contact_action(name, text.operation[2]))
+            case 8:
+                if model.original_book != model.phone_book:
+                    if view.input_request(text.confirm_changes).lower() == 'y':
+                        model.save_file()
+                        view.print_message(text.save_successful)
+                view.print_message(text.exit_program)
+                break
+
+
---
+++ model.py
@@ -0,0 +1,59 @@
+from copy import deepcopy
+
+PATH = 'phones.txt'
+phone_book = {}
+original_book = {}
+
+def open_file():
+    global phone_book, original_book, PATH
+    with open(PATH, 'r', encoding='UTF-8') as file:
+        data = file.readlines()
+    for i, contact in enumerate(data, 1):
+        contact = contact.strip().split(';')
+        phone_book[i] = contact
+    original_book = deepcopy(phone_book)
+
+def save_file():
+    global phone_book, PATH
+    data = []
+    for contact in phone_book.values():
+        contact = ';'.join(contact)
+        data.append(contact)
+    data = '\n'.join(data)
+    with open(PATH, 'w', encoding='UTF-8') as file:
+        file.write(data)
+
+def add_contact(new_contact: list[str]):
+    global phone_book
+    c_id = max(phone_book) + 1
+    phone_book[c_id] = new_contact
+
+
+def find_contact(word: str) -> dict[int, list[str]]:
+    global phone_book
+    result = {}
+    for c_id, contact in phone_book.items():
+        for field in contact:
+            if word.lower() in field.lower():
+                result[c_id] = contact
+                break
+    return result
+
+def edit_contact(c_id: int, new_contact: list[str]):
+    global phone_book
+    current_contact = phone_book.get(c_id)
+    contact = []
+    for i in range(len(new_contact)):
+        if new_contact[i]:
+            contact.append(new_contact[i])
+        else:
+            contact.append(current_contact[i])
+    phone_book[c_id] = contact
+    return contact[0]
+
+def delete_contact(c_id: int) -> str:
+    global phone_book
+    return phone_book.pop(c_id)[0]
+
+
+
